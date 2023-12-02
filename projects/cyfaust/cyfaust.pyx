03ee7b59fabbec367129ac4edbea27218a8d8b87
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2023-12-02 14:48:01 +0300

    snap

--- projects/cyfaust/cyfaust.pyx
+++ projects/cyfaust/cyfaust.pyx
@@ -61,7 +61,8 @@ cdef class InterpreterDspFactory:
 
     def __dealloc__(self):
         if self.ptr and self.ptr_owner:
-            delete_interpreter_dsp_factory(self.ptr)
+            fi.deleteInterpreterDSPFactory(self.ptr)
+            self.ptr = NULL
 
     def get_name(self) -> str:
         """Return factory name."""
@@ -104,13 +105,32 @@ cdef class InterpreterDspFactory:
         """Set a custom memory manager to be used when creating instances"""
         return self.ptr.getMemoryManager()
 
+    def write_to_bitcode(self) -> str:
+        """Write a Faust DSP factory into a bitcode string."""
+        return fi.writeInterpreterDSPFactoryToBitcode(self.ptr).decode()
+
+    def write_to_bitcode_file(self, bit_code_path: str) -> str:
+        """Write a Faust DSP factory into a bitcode file."""
+        return fi.writeInterpreterDSPFactoryToBitcodeFile(
+            self.ptr, bit_code_path.encode('utf8'))
+
     @staticmethod
-    def from_sha(str sha_key) -> InterpreterDspFactory:
+    cdef InterpreterDspFactory from_ptr(fi.interpreter_dsp_factory* ptr, bint owner=False):
+        """Wrap external factory from pointer"""
+        cdef InterpreterDspFactory factory = InterpreterDspFactory.__new__(
+            InterpreterDspFactory)
+        factory.ptr = ptr
+        factory.owner = owner
+        return factory
+
+    @staticmethod
+    def from_sha_key(str sha_key) -> InterpreterDspFactory:
         """create an interpreter dsp factory from a sha key"""
         cdef InterpreterDspFactory factory = InterpreterDspFactory.__new__(
             InterpreterDspFactory)
         factory.ptr_owner = True
-        factory.ptr = <dsp_factory*>get_interpreter_dsp_factory_from_sha_key(sha_key)
+        factory.ptr = <dsp_factory*>fi.getInterpreterDSPFactoryFromSHAKey(
+            sha_key.encode())
         return factory
 
     @staticmethod
@@ -122,7 +142,7 @@ cdef class InterpreterDspFactory:
             InterpreterDspFactory)
         cdef ParamArray params = ParamArray(args)
         factory.ptr_owner = True
-        factory.ptr = <dsp_factory*>create_interpreter_dsp_factory_from_file(
+        factory.ptr = <dsp_factory*>fi.createInterpreterDSPFactoryFromFile(
             filepath.encode('utf8'),
             params.argc,
             params.argv,
@@ -134,7 +154,24 @@ cdef class InterpreterDspFactory:
         return factory
 
     @staticmethod
-    def from_string(str name_app, str code, str args) -> InterpreterDspFactory:
+    def from_bitcode_file(str bit_code_path) -> InterpreterDspFactory:
+        """Create a Faust DSP factory from a bitcode file."""
+        cdef string error_msg
+        error_msg.reserve(4096)
+        cdef InterpreterDspFactory factory = InterpreterDspFactory.__new__(
+            InterpreterDspFactory)
+        factory.ptr_owner = True
+        factory.ptr = <dsp_factory*>fi.readInterpreterDSPFactoryFromBitcodeFile(
+            bit_code_path.encode('utf8'),
+            error_msg,
+        )
+        if error_msg.empty():
+            print(error_msg.decode())
+            return
+        return factory
+
+    @staticmethod
+    def from_string(str name_app, str code, *args) -> InterpreterDspFactory:
         """create an interpreter dsp factory from a string"""
         cdef string error_msg
         error_msg.reserve(4096)
@@ -142,11 +179,28 @@ cdef class InterpreterDspFactory:
             InterpreterDspFactory)
         cdef ParamArray params = ParamArray(args)
         factory.ptr_owner = True
-        factory.ptr = <dsp_factory*>create_interpreter_dsp_factory_from_string(
+        factory.ptr = <dsp_factory*>fi.createInterpreterDSPFactoryFromString(
             name_app.encode('utf8'),
             code.encode('utf8'),
             params.argc,
             params.argv,
+            error_msg,
+        )
+        if error_msg.empty():
+            print(error_msg.decode())
+            return
+        return factory
+
+    @staticmethod
+    def from_bitcode(str bitcode) -> InterpreterDspFactory:
+        """Create a Faust DSP factory from a bitcode string."""
+        cdef string error_msg
+        error_msg.reserve(4096)
+        cdef InterpreterDspFactory factory = InterpreterDspFactory.__new__(
+            InterpreterDspFactory)
+        factory.ptr_owner = True
+        factory.ptr = <dsp_factory*>fi.readInterpreterDSPFactoryFromBitcode(
+            bitcode.encode('utf8'),
             error_msg,
         )
         if error_msg.empty():
@@ -162,6 +216,7 @@ cdef class InterpreterDsp:
     def __dealloc__(self):
         if self.ptr and self.ptr_owner:
             del self.ptr
+            self.ptr = NULL
 
     def __cinit__(self):
         self.ptr = NULL
@@ -223,6 +278,49 @@ cdef class InterpreterDsp:
 
 ctypedef fi.interpreter_dsp_factory dsp_factory
 
+
+def get_dsp_factory_from_sha_key(str sha_key) -> InterpreterDspFactory:
+    """Get the Faust DSP factory associated with a given SHA key."""
+    return InterpreterDspFactory.from_sha_key(sha_key.encode())
+
+def create_dsp_factory_from_file(filename: str, *args) -> InterpreterDspFactory:
+    """Create a Faust DSP factory from a DSP source code as a file."""
+    return InterpreterDspFactory.from_file(filename, *args)
+
+def create_dsp_factory_from_string(name_app: str, code: str, *args) -> InterpreterDspFactory:
+    """Create a Faust DSP factory from a DSP source code as a string."""
+    return InterpreterDspFactory.from_string(name_app, code, *args)
+
+cdef dsp_factory* create_interpreter_dsp_factory_from_signals(
+        const string& name_app, fi.tvec signals, int argc, const char* argv[], string& error_msg):
+    """Create a Faust DSP factory from a vector of output signals."""
+    return fi.createInterpreterDSPFactoryFromSignals(
+        name_app, signals, argc, argv, error_msg)
+
+cdef dsp_factory* create_interpreter_dsp_factory_from_boxes(
+        const string& name_app, fi.Box box, int argc, const char* argv[], string& error_msg):
+    """Create a Faust DSP factory from a box expression."""
+    return fi.createInterpreterDSPFactoryFromBoxes(
+        name_app, box, argc, argv, error_msg)
+
+def delete_all_interpreter_dsp_factories():
+    """Delete all Faust DSP factories kept in the library cache."""
+    fi.deleteAllInterpreterDSPFactories()
+
+cdef vector[string] get_all_interpreter_dsp_factories():
+    """Return Faust DSP factories of the library cache as a vector of their SHA keys."""
+    return fi.getAllInterpreterDSPFactories()
+
+def start_multithreaded_access_mode() -> bool:
+    """Start multi-thread access mode"""
+    return fi.startMTDSPFactories()
+
+def stop_multithreaded_access_mode():
+    """Stop multi-thread access mode."""
+    fi.stopMTDSPFactories()
+
+## api -------------------------------------------------------------------
+
 cdef dsp_factory* get_interpreter_dsp_factory_from_sha_key(str sha_key):
     """Get the Faust DSP factory associated with a given SHA key."""
     return fi.getInterpreterDSPFactoryFromSHAKey(sha_key.encode())
@@ -239,61 +337,23 @@ cdef dsp_factory* create_interpreter_dsp_factory_from_string(
     return fi.createInterpreterDSPFactoryFromString(
         name_app, dsp_content, argc, argv, error_msg)
 
-cdef dsp_factory* create_interpreter_dsp_factory_from_signals(
-        const string& name_app, fi.tvec signals, int argc, const char* argv[], string& error_msg):
-    """Create a Faust DSP factory from a vector of output signals."""
-    return fi.createInterpreterDSPFactoryFromSignals(
-        name_app, signals, argc, argv, error_msg)
-
-cdef dsp_factory* create_interpreter_dsp_factory_from_boxes(
-        const string& name_app, fi.Box box, int argc, const char* argv[], string& error_msg):
-    """Create a Faust DSP factory from a box expression.
-    """
-    return fi.createInterpreterDSPFactoryFromBoxes(
-        name_app, box, argc, argv, error_msg)
-
 cdef bint delete_interpreter_dsp_factory(dsp_factory* factory):
-    """Delete a Faust DSP factory.
-    """
+    """Delete a Faust DSP factory."""
     return fi.deleteInterpreterDSPFactory(factory)
 
-cdef void delete_all_interpreter_dsp_factories():
-    """Delete all Faust DSP factories kept in the library cache.
-    """
-    fi.deleteAllInterpreterDSPFactories()
-
-cdef vector[string] get_all_interpreter_dsp_factories():
-    """Return Faust DSP factories of the library cache as a vector of their SHA keys.
-    """
-    return fi.getAllInterpreterDSPFactories()
-
-cdef bint start_multithreaded_access_mode():
-    """Start multi-thread access mode
-    """
-    return fi.startMTDSPFactories()
-
-cdef void stop_multithreaded_access_mode():
-    """Stop multi-thread access mode.
-    """
-    fi.stopMTDSPFactories()
-
 cdef dsp_factory* read_interpreter_dsp_factory_from_bitcode(const string& bitcode, string& error_msg):
-    """Create a Faust DSP factory from a bitcode string.
-    """
+    """Create a Faust DSP factory from a bitcode string."""
     return fi.readInterpreterDSPFactoryFromBitcode(bitcode, error_msg)
-    
-cdef string write_interpreter_dsp_factory_to_bitcode(dsp_factory* factory):
-    """Write a Faust DSP factory into a bitcode string.
-    """
-    return fi.writeInterpreterDSPFactoryToBitcode(factory)
 
 cdef dsp_factory* read_dsp_factory_from_bitcode_file(const string& bit_code_path, string& error_msg):
-    """Create a Faust DSP factory from a bitcode file.
-    """
+    """Create a Faust DSP factory from a bitcode file."""
     return fi.readInterpreterDSPFactoryFromBitcodeFile(bit_code_path, error_msg)
 
+cdef string write_interpreter_dsp_factory_to_bitcode(dsp_factory* factory):
+    """Write a Faust DSP factory into a bitcode string."""
+    return fi.writeInterpreterDSPFactoryToBitcode(factory)
+
 cdef bint write_interpreter_dsp_factory_to_bitcode_file(dsp_factory* factory, const string& bit_code_path):
-    """Write a Faust DSP factory into a bitcode file.
-    """
+    """Write a Faust DSP factory into a bitcode file."""
     return fi.writeInterpreterDSPFactoryToBitcodeFile(factory, bit_code_path)
 
